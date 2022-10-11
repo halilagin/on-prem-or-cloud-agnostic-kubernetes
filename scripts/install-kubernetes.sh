5b25cb65ccf43ac138f0d666ec5b7d7a550342dc
halilagin <halil.agin@gmail.com>
2022-10-11 22:59:26 +0000

    disable ipv6

--- scripts/install-kubernetes.sh
+++ scripts/install-kubernetes.sh
@@ -24,6 +24,14 @@ swapoff -a
 rm /swap.img
 sed "-i.bak" '/swap.img/d' /etc/fstab
 hostnamectl set-hostname kube_master
+cat <<EOF >>/etc/sysctl.conf
+net.ipv6.conf.all.disable_ipv6=1
+net.ipv6.conf.default.disable_ipv6=1
+net.ipv6.conf.lo.disable_ipv6=1
+EOF
+sysctl -p
+echo /etc/init.d/procps restart >> /etc/rec.local
+chmod 755 /etc/rc.local
 
 
 echo "installing kubernetes"
