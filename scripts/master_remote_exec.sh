bab7bb698fa611b7f10afbbec90abeca1463e447
Ubuntu <ubuntu@ip-10-0-2-106.us-east-2.compute.internal>
2022-10-21 23:29:34 +0000

    fix master exec script

--- scripts/master_remote_exec.sh
+++ scripts/master_remote_exec.sh
@@ -1,5 +1,5 @@
 repo_root=on-prem-or-cloud-agnostic-kubernetes
 #sudo bash -c "echo ${self.private_ip} kubemaster >>/etc/hosts" 
-cd ~/${repo_root}/scripts && sudo sh install-kubernetes.sh >/tmp/kube_install.log 2>&1 &
+cd ~/${repo_root}/scripts && sudo sh install-kubernetes.sh >/tmp/kube_install.log 2>&1 
 sh ~/${repo_root}/scripts/kubernetes_master_postinstall.sh
 
